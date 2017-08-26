1511a05ce7bf28053351d8a61722eb46e985e38d
lakshmipriyaramjeeban <31353769+lakshmipriyaramjeeban@users.noreply.github.com>
2017-08-26 11:52:44 +0530

    Create problem6.cpp

---
+++ problem6.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+float r,d,c,a;
+cout<<"enter the radiuus of the circle:";
+cin>>r;
+d=2*r;
+cout<<"diameter="<<d;
+c=2*3.141*r;
+cout<<"circumference="<<c;
+a=3.141*r*r;
+cout<<"area="<<a;
+	return 0;
+}
