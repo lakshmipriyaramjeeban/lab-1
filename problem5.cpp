2a6e7c5ad4c2713e35bf7223bd168ff1170c5ead
lakshmipriyaramjeeban <31353769+lakshmipriyaramjeeban@users.noreply.github.com>
2017-08-26 11:39:45 +0530

    Rename problem4.cpp to problem5.cpp

---
+++ problem5.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int l,b,c;
+	cout<<"enter the length of the rectangle";
+	cin>>l;
+	cout<<"enter the breadth of the rectangle";
+	cin>>b;
+	c=l*b;
+	cout<<"area="<<c;
+	return 0;
+}
