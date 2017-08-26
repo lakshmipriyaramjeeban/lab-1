6bbd4cadf8079b7544979ec5745aab3b1e744781
lakshmipriyaramjeeban <31353769+lakshmipriyaramjeeban@users.noreply.github.com>
2017-08-26 11:10:22 +0530

    Create problem3.cpp

---
+++ problem3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int a,b,c,d,e,f,g;
+	cout<<"enter first number a:";
+	cin>>a;
+	cout<<"enter second number b:";
+	cin>>b;
+	c=a+b;
+	d=a-b;
+	e=a*b;
+	f=a/b;
+	g=a%b;
+	cout<<"addition="<< c<< endl<< "subtraction="<< d<<endl<<"multiplication="<<e<<endl<<"division="<<f<<endl<<"modulus="<<g<<endl;
+
+	return 0;
+}
