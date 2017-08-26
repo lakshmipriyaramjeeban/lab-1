3b1cb57346e27936308b50f40d6983ccce3c76d3
lakshmipriyaramjeeban <31353769+lakshmipriyaramjeeban@users.noreply.github.com>
2017-08-26 10:25:35 +0530

    Create problem1.cpp

---
+++ problem1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int a;
+	float b;
+	char c;
+	bool d=true;
+	bool e=false;
+	cout << "Enter Integer:"<<endl;
+	cin >> a;
+	cout << "Integer=" << a << endl;
+	cout << "enter Float" << endl;
+	cin >> b;
+	cout << "Float=" << b<<endl;
+	cout << "Enter Character:" << endl;
+	cin >> c;
+	cout << "character=" << c<<endl;
+	cout << d << endl;
+	cout << e;
+	return 0;
+}
